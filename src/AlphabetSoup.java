412ce7c7df9a82e8e0fbf8bb9029e14ccf5b2548
Gennaro <gen.marrazzo@gmail.com>
2019-03-05 22:14:06 +0100

    linear

--- src/AlphabetSoup.java
+++ src/AlphabetSoup.java
@@ -6,7 +6,8 @@ public class AlphabetSoup {
 		// M is the dimension of the message and S is the dimension of the bowl of letters
 		// the complexity in term of computational time is O(MxS), this because I have put two 'cicle for' that 
 		// run through the two input string and each of this 'for' has a complexity proportional to the input dimension (M or S)
-
+		//String a = "   of David is";
+		//String a = "egassem";
 		String a = "Hi,   message of David is"; 
 		//String bowl = "EgaSsEmoff"; /*false result*/
 		String bowl = "message,abcdefghjFHiGTUOIKJofisDavidHFGREJvdser"; /*true result*/
@@ -23,24 +24,22 @@ public class AlphabetSoup {
 
 		if (message.length() > bowlOfAlphabetSoup.length())
 			return result; // there are not enough letters into the bowl to compose the message
+		int i = 0;
 
-		for (int i = 0, m = message.length(); i < m; i++) {
+		for (int j = 0, s = bowlOfAlphabetSoup.length(); j < s && i < message.length(); j++) {
 
 			char checkChar = message.charAt(i);
-
-			for (int j = 0, s = bowlOfAlphabetSoup.length(); j < s; j++) {
-
-				if (String.valueOf(checkChar).equalsIgnoreCase(String.valueOf(bowlOfAlphabetSoup.charAt(j)))){
-					String found = String.valueOf(bowlOfAlphabetSoup.charAt(j));  
-					result = true;
-					bowlOfAlphabetSoup = bowlOfAlphabetSoup.replaceFirst(found, "");
-					j = s;
-				}
-				else{
-					if (j == s-1) {
-						result = false;
-						return result;
-					}
+			if (String.valueOf(checkChar).equalsIgnoreCase(String.valueOf(bowlOfAlphabetSoup.charAt(j)))){
+				String found = String.valueOf(bowlOfAlphabetSoup.charAt(j));  
+				result = true;
+				bowlOfAlphabetSoup = bowlOfAlphabetSoup.replaceFirst(found, "");
+				j=-1;
+				i++;
+			}
+			else{
+				if (j == s-1) {
+					result = false;
+					return result;
 				}
 			}
 		}
